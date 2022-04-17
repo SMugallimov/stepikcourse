328e98544bcd876034a444a05cdcb55e0d04fb4c
smugallilmov <smugallimov@mail.ru>
2022-04-17 16:54:13 +0400

    Homework 2.2: Next and Previous

---
+++ src/com/company/homework2_2/NextPrevious.java
@@ -0,0 +1,15 @@
+package com.company.homework2_2;
+
+import java.util.Scanner;
+
+public class NextPrevious {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a number.");
+
+        int number = in.nextInt();
+        int nextNumber = 1;
+        System.out.println("The next number for the number " + number + " is " + (nextNumber + number) + ".");
+        System.out.println("The previous number for the number " + number + " is " + --number + ".");
+    }
+}
