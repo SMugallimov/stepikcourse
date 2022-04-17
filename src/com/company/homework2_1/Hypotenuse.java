ef1aba38fcced046495e5781dd76101280b8f6a9
smugallilmov <smugallimov@mail.ru>
2022-04-17 16:30:54 +0400

    Homework 2.1: Hypotenuse

---
+++ src/com/company/homework2_1/Hypotenuse.java
@@ -0,0 +1,17 @@
+package com.company.homework2_1;
+
+import java.util.Scanner;
+
+public class Hypotenuse {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input two numbers: ");
+
+        int number1 = in.nextInt();
+        int number2 = in.nextInt();
+        int number3 = (int)Math.sqrt(Math.pow(number1, 2) + Math.pow(number2, 2));
+
+        System.out.println(number3);
+    }
+}
