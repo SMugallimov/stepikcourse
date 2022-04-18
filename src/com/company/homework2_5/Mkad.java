17f71fc7d504c827c32c6df8b69e4de6fd620a79
smugallilmov <smugallimov@mail.ru>
2022-04-18 22:30:40 +0400

    Homework 2.5: MKAD

---
+++ src/com/company/homework2_5/Mkad.java
@@ -0,0 +1,16 @@
+package com.company.homework2_5;
+
+import java.util.Scanner;
+
+public class Mkad {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int speed = in.nextInt();
+        int time = in.nextInt();
+
+        System.out.println(((speed * time) % 109 + 109) % 109);
+
+        in.close();
+    }
+}
