59874872d7baa0f706d8fcf2d9cff3efc7cba5ed
smugallilmov <smugallimov@mail.ru>
2022-04-26 21:58:13 +0400

    Homework 2.1: Apples

--- src/com/company/homework2_1/Apples1.java
+++ src/com/company/homework2_1/Apples1.java
@@ -1,8 +1,8 @@
-package com.company.homework2_3;
+package com.company.homework2_1;
 
 import java.util.Scanner;
 
-public class Apples {
+public class Apples1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int students = in.nextInt();
