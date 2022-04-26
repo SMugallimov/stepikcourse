dd3079a5f3596362f489a748306d7ebaee121f13
smugallilmov <smugallimov@mail.ru>
2022-04-26 22:02:40 +0400

    Homework 2.1: Number of tenth

--- src/com/company/homework2_1/NumberOfTenth1.java
+++ src/com/company/homework2_1/NumberOfTenth1.java
@@ -1,9 +1,10 @@
-package com.company.homework2_7;
+package com.company.homework2_1;
 
 import java.util.Scanner;
 
-public class NumberOfTenth {
+public class NumberOfTenth1 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
 
